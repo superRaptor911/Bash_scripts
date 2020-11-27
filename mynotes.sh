f2ca6daa59b359033a8208b51a53bc1d564c779b
superRaptor911 <raptor.inc2018@gmail.com>
2020-11-27 14:59:32 +0530

    Enhanced mynotes.sh

--- mynotes.sh
+++ mynotes.sh
@@ -2,9 +2,34 @@
 
 arg1=$1
 file="note.txt"
+edit=0
+
+args=("$@")
+
+for (( i=0; i<$#; i++ ))
+do
+	case "${args[$i]}" in
+	-e)
+		edit=1
+		;;
+	-f)
+		let i=$i+1
+		file="${args[$i]}"
+
+		if [ "$file" == "" ]; then
+			echo "Error : empty file name"
+			exit 2
+		fi
+		;;
+    *)
+        echo "wrong args"
+        exit 2
+     esac
+done
 
 
-if [ "$arg1" == "-edit" ] || [ "$arg1" == "-e" ]; then
+
+if [ $edit -eq 1 ]; then
 	micro ~/Documents/$file
 else
 	cat ~/Documents/$file
