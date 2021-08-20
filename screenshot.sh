195113c601774f4c5a2708608ee89d51efec30c9
superRaptor911 <raptor.inc2018@gmail.com>
2021-08-20 20:15:49 +0530

    Updated screenshot && deleted unused scripts

--- screenshot.sh
+++ screenshot.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
+mkdir -p ~/Pictures/screenshots
+file_name=$(echo 'image.png' | dmenu -p 'Save file as')
+file_path=~/Pictures/screenshots/$file_name
 
 if [ "$1" == "-s" ]; then
-	# OSX-like area selection screenshots
-    file_name=$(echo 'image.png' | dmenu -p 'Save file as')
-    scrot --line style=dash,width=3,color="red" -s $file_name -e 'mkdir -p ~/Pictures/screenshots && mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/`ls -1 -t ~/Pictures/screenshots | head -1`' # Area selection
-
-elif [ "$1" == "-ds" ]; then
-    scrot --line style=dash,width=3,color="red" -s $file_name -e 'mkdir -p ~/Pictures/screenshots &&
-        mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i
-            ~/Pictures/screenshots/`ls -1 -t ~/Pictures/screenshots | head -1`' # Area selection
-
+    import $file_path 
 else
-    file_name=$(echo 'image.png' | dmenu -p 'Save file as')
-    scrot $file_name -e 'mkdir -p ~/Pictures/screenshots && mv $f ~/Pictures/screenshots && xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/`ls -1 -t ~/Pictures/screenshots | head -1`' # All screens
+    import -window root $file_path
 fi
 
+xclip -selection clipboard -t image/png -i ~/Pictures/screenshots/$file_name
 notify-send "Saving screenshot as" $file_name
